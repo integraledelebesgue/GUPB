1fee4c6cb9e79de070d28077010f0dfc44299b27
rpiwowar02 <115703243+rpiwowar02@users.noreply.github.com>
2025-04-11 00:46:22 +0200

    Pathfinder & heuristics
    Overengineered for Survival
    Pathfinder
    heuristics
    Thompson Sampling

---
+++ gupb/controller/rustler/goal.py
@@ -0,0 +1,26 @@
+from gupb.model import characters
+from gupb.model import coordinates
+
+class Goal:
+    def __init__(self, name: str, priority: int, target_cords: coordinates.Coords, vanishable: bool = False, facing :characters.Facing =None):
+        self.name = name
+        self.priority = priority
+        self.journey_target = target_cords
+        self.vanishable = vanishable # True if the goal should be deleted when stepped on 
+        self.facing = facing
+
+    def __eq__(self, other):
+        if isinstance(other, Goal):
+            return (
+                self.name == other.name and 
+                self.priority == other.priority and 
+                self.journey_target == other.journey_target
+            )
+        return False
+
+    def __hash__(self):
+        return hash((self.name, self.priority, self.journey_target))
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
